9c268d014ea916dc21ac2fc548a36c68bba1e80e
Brant Martin <brantmartin473@gmail.com>
2022-04-16 15:44:43 -0700

    finished the first test file in employeeTest

--- __tests__/EmployeeTest.js
+++ __tests__/EmployeeTest.js
@@ -9,3 +9,23 @@ test("Check Employee ID", function(){
     var testObject = new Employee("Bryan",123)
     expect(testObject.id).toBe(123)
 })
+
+test("Check Employee email", function(){
+    var testObject = new Employee("Bryan",123 ,'junk@junk.com')
+    expect(testObject.email).toBe('junk@junk.com')
+})
+
+test("Check getName", function(){
+    var testObject = new Employee("Bryan")
+    expect(testObject.getName()).toBe('Bryan')
+})
+
+test("Check Employee ID", function(){
+    var testObject = new Employee("Bryan",123)
+    expect(testObject.getId()).toBe(123)
+})
+
+test("Check Employee email", function(){
+    var testObject = new Employee("Bryan",123 ,'junk@junk.com')
+    expect(testObject.getEmail()).toBe('junk@junk.com')
+})
